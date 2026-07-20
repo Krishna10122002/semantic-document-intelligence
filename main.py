e7b8f666beca0c776edbbbf953e5adcf0d98199a
Krishna10122002 <159173909+Krishna10122002@users.noreply.github.com>
2026-07-20 11:37:04 +0530

    Implement FastAPI endpoints for document handling

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
+from fastapi import FastAPI, UploadFile, File
 
+from app.utils import save_file
+from app.config import UPLOAD_FOLDER
+
+from app.rag import process_document
+from app.rag import ask_question
+
+app = FastAPI(
+    title="Semantic Document Intelligence Platform"
+)
+
+@app.get("/")
+def home():
+    return {
+        "message":"Semantic Document Intelligence Platform"
+    }
+
+
+@app.post("/upload")
+
+async def upload(file: UploadFile = File(...)):
+
+    filepath = save_file(file, UPLOAD_FOLDER)
+
+    process_document(filepath)
+
+    return {
+        "status":"Document Indexed"
+    }
+
+
+@app.get("/ask")
+
+def ask(question:str):
+
+    answer = ask_question(question)
+
+    return {
+        "question":question,
+        "answer":answer
+    }
